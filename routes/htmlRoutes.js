e6083dd4c37e68a2ac28c26039cda8f9a113eee2
willy <willygodwin47@gmail.com>
2021-02-08 22:03:02 +0800

    Added comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+//Sends the HTML file when a get request is received
 router.get('/notes',(req,res) => {
     const resPath = path.join(__dirname,'..','public','notes.html');
     res.sendFile(resPath);
